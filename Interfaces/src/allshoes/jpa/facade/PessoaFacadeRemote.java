47e66879fe3446a16b1d6988caefe97f729ba8f7
Bruno <Bruno@192.168.1.100>
2013-03-17 14:45:15 -0300

    Criação das interfaces remotas PessoaFacadeRemote e UsuarioFacadeRemote

---
+++ Interfaces/src/allshoes/jpa/facade/PessoaFacadeRemote.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package allshoes.jpa.facade;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author Bruno
+ */
+@Remote
+public interface PessoaFacadeRemote {
+    
+}
