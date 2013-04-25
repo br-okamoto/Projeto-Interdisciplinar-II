e208e67e572a8622b6a7ef0ae5101d1647874495
41081455 <41081455@093168-P31S304.macklabs.br>
2013-04-24 22:06:36 -0300

    Alteração: (find Usuario) para (find Funcionario).

--- Filial/src/controller/FilialController.java
+++ Filial/src/controller/FilialController.java
@@ -1,8 +1,8 @@
 
 package controller;
 
-import allshoes.jpa.Usuario;
-import allshoes.jpa.facade.UsuarioFacadeRemote;
+import allshoes.jpa.Funcionario;
+import allshoes.jpa.facade.FuncionarioFacadeRemote;
 import java.util.List;
 import java.util.Properties;
 import javax.naming.InitialContext;
@@ -10,26 +10,26 @@ import javax.naming.InitialContext;
 
 public class FilialController {
 
-    private UsuarioFacadeRemote usuarioFacade;
+    private FuncionarioFacadeRemote funcionarioFacade;
 
     public FilialController() throws Exception {
         Properties props = new Properties();
         props.load(new java.io.FileInputStream("jndi.properties"));
         InitialContext ctx = new InitialContext(props);
-        usuarioFacade = (UsuarioFacadeRemote) ctx.lookup("ejb/UsuarioFacade");
+        funcionarioFacade = (FuncionarioFacadeRemote) ctx.lookup("ejb/FuncionarioFacade");
     }
 
-    public void create(Usuario usuario) {
-        usuarioFacade.create(usuario);
+    public void create(Funcionario funcionario) {
+        funcionarioFacade.create(funcionario);
     }
 
-    public Usuario find(String username){
-        return usuarioFacade.find(username);
+    public Funcionario find(String username){
+        return funcionarioFacade.find(username);
     }
             
             
-    public List<Usuario> findAll() {
-        return (usuarioFacade.findAll());
+    public List<Funcionario> findAll() {
+        return (funcionarioFacade.findAll());
     }
     
     
