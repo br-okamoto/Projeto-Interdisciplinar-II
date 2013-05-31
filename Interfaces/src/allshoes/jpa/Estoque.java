33af667c852100654c9d6aae8e653893017a21e8
rodrigo <rodrigo@rodrigo-PC>
2013-05-31 16:03:54 -0300

    Classe Estoque alterado

--- Interfaces/src/allshoes/jpa/Estoque.java
+++ Interfaces/src/allshoes/jpa/Estoque.java
@@ -1,28 +1,26 @@
 package allshoes.jpa;
 
 import java.io.Serializable;
-import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
 
 
 @Entity
 public class Estoque implements Serializable, Cloneable {
 
-    private static final long serialVersionUID = 1L;
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idEstoque;
     
-    //@ManyToMany(cascade = CascadeType.PERSIST,fetch=FetchType.EAGER)
-    //private List<Produto> produto;
+    private String observacao;
 
+    public Estoque(){
+    
+    }
+    
     public int getIdEstoque() {
         return idEstoque;
     }
@@ -31,13 +29,15 @@ public class Estoque implements Serializable, Cloneable {
         this.idEstoque = idEstoque;
     }
 
-    /*public List<Produto> getProduto() {
-        return produto;
+    public String getObservacao() {
+        return observacao;
     }
 
-    public void setProduto(List<Produto> produto) {
-        this.produto = produto;
-    }*/
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    
+    
     
     
 }
